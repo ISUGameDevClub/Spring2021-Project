03d88e047b301cabf73d835bde3f3d213c486969
Jason Kress <greenmegaman33@gmail.com>
2021-02-03 21:52:03 -0600

    Movement and health

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public PlayerMovement pm;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            pm.isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            pm.isGrounded = false;
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth;
+    public int currHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HealDamage(int heal)
+    {
+        currHealth += heal;
+
+        if (currHealth > maxHealth)
+        {
+            currHealth = maxHealth;
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        currHealth -= damage;
+        
+        if (currHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
