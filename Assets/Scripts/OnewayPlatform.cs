4a04282c82615dafe93ff82c6e77cabc7ad235eb
Jason Kress <greenmegaman33@gmail.com>
2021-02-10 21:30:27 -0600

    One way platform

---
+++ Assets/Scripts/OnewayPlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnewayPlatform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    private float waitTime;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            waitTime = 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if(waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.5f;
+            }
+           
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            effector.rotationalOffset = 0;
+        }
+        if(waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+            
+        }
+    }
+}
