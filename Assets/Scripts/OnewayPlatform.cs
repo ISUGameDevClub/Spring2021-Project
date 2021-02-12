5d6ba25ed2870850b61b961513912c3d0c5203f9
bhayden51 <bradley8451@hotmail.com>
2021-02-12 10:53:54 -0600

    Small tweaks to player movement variables

--- Assets/Scripts/OnewayPlatform.cs
+++ Assets/Scripts/OnewayPlatform.cs
@@ -6,46 +6,35 @@ public class OnewayPlatform : MonoBehaviour
 {
     private GameObject player;
     private Collider2D col;
-    private PlatformEffector2D effector;
     private float waitTime;
     
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>().gameObject;
-        effector = GetComponent<PlatformEffector2D>();
         col = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.S))
-        {
-            waitTime = 0.5f;
-        }
-
         if (Input.GetKey(KeyCode.S))
         {
-            if(waitTime <= 0)
-            {
-                waitTime = 0.5f;
-            }
-           
+            waitTime = 0.5f;         
         }
 
-        if (player.transform.position.y > transform.position.y + 1.45f && waitTime <= 0)
+        if(waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+        }
+
+        if (player.transform.position.y > transform.position.y + 1.35f && waitTime <= 0)
         {
             col.enabled = true;
         }
         else
         {
             col.enabled = false;
-        }
-
-        if(waitTime > 0)
-        {
-            waitTime -= Time.deltaTime;
         }
     }
 }
