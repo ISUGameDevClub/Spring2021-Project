05a15e3f289aebb285f38fbe27cfa1c107956b2e
Andrew Richmond <sk8erpro1@gmail.com>
2021-02-13 17:04:42 -0600

    Dash ability

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float jumpPower;
+    public bool isGrounded;
+    public float gravity;
+    public float fallSpeed;
+
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+
+        if(!isGrounded && rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.gravityScale = gravity * fallSpeed;
+        }
+        else
+        {
+            rb.gravityScale = gravity;
+        }
+
+    }
+
+    private void Movement()
+    {
+       transform.Translate(new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed, 0));
+    }
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+    }
+}
