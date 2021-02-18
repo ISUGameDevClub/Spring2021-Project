a5c9ccf5036bbcba71365610256c42cfb70a5528
Dylan Benoit <dylanbenoit20@gmail.com>
2021-02-17 20:15:12 -0600

    merging

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth;
+    public int curHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    public void HealDamage(int heal)
+    {
+        curHealth += heal;
+
+        if(curHealth > maxHealth)
+        {
+            curHealth = maxHealth;
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        curHealth -= damage;
+
+        if(curHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
