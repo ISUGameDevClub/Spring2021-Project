f0ccb38295c4b627aab2131fdf6901a249c850a5
AWAIS NADEEM <anadeem@ilstu.edu>
2021-02-17 20:32:12 -0600

    Scene transitiions

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChange : MonoBehaviour
+{
+    public int scene;
+
+    public float transitionTime = 1f;
+
+    public Animator transition;
+    private void OnTriggerEnter2D()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(scene);
+    }
+}
