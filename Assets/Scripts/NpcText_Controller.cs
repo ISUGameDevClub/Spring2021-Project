114fde2384753781e15b61e8020569046dc046c8
Jason Kress <greenmegaman33@gmail.com>
2021-03-10 21:26:11 -0600

    NPC text

--- Assets/Scripts/NpcText_Controller.cs
+++ Assets/Scripts/NpcText_Controller.cs
@@ -2,29 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class NpcText : MonoBehaviour
+public class NpcText_Controller : MonoBehaviour
 {
-    public string textShown;
-    public float lengthShown;
-    public Text myText;
-    public GameObject textBox;
+    private Text myText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
-        {
-            ShowText(textShown, lengthShown);
-        }
     }
     public void ShowText(string text, float timeShown)
     {
@@ -34,9 +26,7 @@ public class NpcText : MonoBehaviour
     private IEnumerator TextTime(string shownText, float timeShown)
     {
         myText.text = shownText;
-        textBox.SetActive(true);
         yield return new WaitForSeconds(timeShown);
-        textBox.SetActive(false);
         myText.text = "";
     }
 }
