6923dd048985dd2a67310fccd10497561782c578
Dylan Benoit <dylanbenoit20@gmail.com>
2021-03-10 22:00:01 -0600

    Added upgraded ammo and gold, and shop

--- Assets/Scripts/GoldSystem.cs
+++ Assets/Scripts/GoldSystem.cs
@@ -7,23 +7,40 @@ public class GoldSystem : MonoBehaviour
 {
     public int totalGold;
     public Text goldText;
+    public bool upgraded;
+    public int maxDefaultGold;
+    public int maxUpgradedGold;
 
     // Start is called before the first frame update
     void Start()
     {
+        upgraded = false;
         totalGold = 0;
         goldText.text = "Coins: " + totalGold;
     }
 
     public void AddGold(int amount)
     {
-        totalGold+= amount;
-        goldText.text = "Coins: " + totalGold;
+        if (upgraded == false && totalGold < maxDefaultGold)
+        {
+            totalGold += amount;
+            goldText.text = "Coins: " + totalGold;
+        }
+        else if (upgraded == true && totalGold < maxUpgradedGold)
+        {
+            totalGold += amount;
+            goldText.text = "Coins: " + totalGold;
+        }
     }
 
     public void SubtractGold(int amount)
     {
         totalGold-= amount;
         goldText.text = "Coins: " + totalGold;
+    }
+
+    public void setUpgradeGold()
+    {
+        upgraded = true;
     }
 }
---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    private GoldSystem gs;
+    private AmmoSystem ammoSys;
+
+    public int ammoPouchUpgradeCost;
+    public int goldPouchUpgradeCost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammoSys = FindObjectOfType<AmmoSystem>();
+        gs = FindObjectOfType<GoldSystem>();
+        Shop[] shops = FindObjectsOfType<Shop>();
+        for(int i = 0; i < shops.Length; i++)
+        {
+            shops[i].shopUI = gameObject;
+        }
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateShop()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void BuyAmmoPouchUpgrade()
+    {
+        if(gs.totalGold >= ammoPouchUpgradeCost && ammoSys.upgraded == false)
+        {
+            gs.SubtractGold(ammoPouchUpgradeCost);
+            ammoSys.setUpgradeAmmo();
+        }
+        
+    }
+
+    public void BuyGoldPouchUpgrade()
+    {
+        if (gs.totalGold >= goldPouchUpgradeCost && gs.upgraded == false)
+        {
+            gs.SubtractGold(goldPouchUpgradeCost);
+            gs.setUpgradeGold();
+        }
+    }
+
+    public void DeactivateShop()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+}
