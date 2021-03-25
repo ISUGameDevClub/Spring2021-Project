2403216efc8146e5c60570cb4499d986df80f145
Tadas Rimkevicius <rimketad000@s.lhs210.net>
2021-03-24 20:05:38 -0500

    Rooftop Level Major Progress

---
+++ Assets/Scripts/GrapplingPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrapplingPoint : MonoBehaviour
+{
+    private Animator anim;
+    public Transform point1;
+    public Transform point2;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    
+    }
+
+    //anim.SetTrigger("Swing1");
+}
