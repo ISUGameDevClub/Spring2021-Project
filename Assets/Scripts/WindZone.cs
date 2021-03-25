ad5165c97b9aeb6708cffdb147b32f16dfb5afb0
Dylan Benoit <dylanbenoit20@gmail.com>
2021-03-24 21:41:56 -0500

    Finished WindZone script and prefab

--- Assets/Scripts/WindZone.cs
+++ Assets/Scripts/WindZone.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class WindZone : MonoBehaviour
 {
+    public float seconds;
+    public bool constantWind = true;
     public float windPower;
+    private Coroutine timerCoroutine;
     private bool inWindZone;
-    private Rigidbody2D rb;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
     // Update is called once per frame
@@ -18,13 +21,38 @@ public class WindZone : MonoBehaviour
     {
         
     }
+    
+    public IEnumerator Timer(float seconds)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(seconds);
+            
+             for (int i = 0; i < 60; i++)
+                { 
+                    if (inWindZone)
+                    {
+                        player.transform.Translate(new Vector2(windPower / 60, 0));
+                    }
+                yield return new WaitForSeconds(1f / 60f);
+                }
+            
+        }
+    }
 
     private void FixedUpdate()
     {
-        if (isGrounded && rb.velocity.y < .01f)
-            rb.velocity = rb.velocity * new Vector2(0, 1);
-        if (canMove)
-            Movement();
+        if (constantWind)
+        {
+            if (inWindZone)
+            {
+                player.transform.Translate(new Vector2(1 * Time.deltaTime * windPower, 0));
+            }
+        }
+        else
+        {
+            if(timerCoroutine == null) timerCoroutine = StartCoroutine(Timer(seconds));
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
