e460cf192a10352a7d208a259f5a4016a75016ac
bhayden51 <bradley8451@hotmail.com>
2021-04-02 14:41:33 -0500

    Enemy animations

--- Assets/Scripts/RangedEnemy.cs
+++ Assets/Scripts/RangedEnemy.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public class RangedEnemy : MonoBehaviour
 {
+    public SpriteRenderer sr;
     public float timeBetweenShots;
     public GameObject bullet;
     private GameObject player;
     private float shotTimer;
+    private Animator myAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>().gameObject;
+        myAnim = GetComponent<Animator>();
         shotTimer = timeBetweenShots;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (player.transform.position.x > transform.position.x)
+            sr.flipX = true;
+        else
+            sr.flipX = false;
+
         if (shotTimer > 0)
             shotTimer -= Time.deltaTime;
         else
         {
+            myAnim.SetTrigger("Attack");
             shotTimer = timeBetweenShots;
             if (player.transform.position.x > transform.position.x)
             {
