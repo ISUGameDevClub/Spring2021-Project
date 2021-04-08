c660efe33841a3b61e79c958e1c4da75c0d5bf13
Pian Doerr <cody.doerr@yahoo.com>
2021-04-07 20:39:22 -0500

    Pushing again

---
+++ Assets/Scripts/Fuel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fuel : MonoBehaviour
+{
+    public static int fuelCount;
+    public Text fuelText;
+    public float destroyAfter;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Fuel")
+        {
+            fuelCount++;
+            Destroy(collision.gameObject);
+        }
+    }
+    private void Start()
+    {
+        if(fuelText != null)
+            fuelText.text = "Fuel: " + fuelCount;
+    }
+    private void FixedUpdate()
+    {
+        if (fuelText != null)
+            fuelText.text = "Fuel: " + fuelCount;
+    }
+
+}
